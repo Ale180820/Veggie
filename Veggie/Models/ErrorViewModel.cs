278fa79a0d010f2f160d4cb23d99a010553a4af1
aylinne recinos <aylinneart@gmail.com>
2020-11-10 20:53:24 -0600

    First commit

---
+++ Veggie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Veggie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
