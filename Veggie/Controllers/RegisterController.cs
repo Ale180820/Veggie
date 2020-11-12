a603f3091d8dd0189dd4020ec62467512063ae10
victorisimoo <vmelendrez2013241@gmail.com>
2020-11-12 07:13:15 -0600

    login module

---
+++ Veggie/Controllers/RegisterController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Veggie.Controllers {
+    public class RegisterController : Controller {
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
--- VeggieAPI/Models/MongoHelper.cs
+++ VeggieAPI/Models/MongoHelper.cs
@@ -1,20 +1,19 @@
 ﻿using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace VeggieAPI.Models {
 
     public class MongoHelper {
 
         public static IMongoClient client { get; set; }
-        public static IMongoDatabase database { get; set }
+        public static IMongoDatabase database { get; set; }
         public static string MongoConnection = "mongodb+srv://veggie_user:pass123@veggie.ioh5v.mongodb.net/<dbname>?retryWrites=true&w=majority";
         public static string MongoDatabase = "VeggieDB";
 
         internal static void ConnectToMongoService() {
             try {
+
                 client = new MongoClient(MongoConnection);
                 database = client.GetDatabase(MongoDatabase);
 
