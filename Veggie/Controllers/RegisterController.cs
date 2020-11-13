bd69886a65de4b1630c83301579acd16383e5420
victorisimoo <vmelendrez2013241@gmail.com>
2020-11-12 23:54:02 -0600

    update branch

--- Veggie/Controllers/RegisterController.cs
+++ Veggie/Controllers/RegisterController.cs
@@ -1,14 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using VeggieAPI.Controllers;
+using VeggieBack.Models;
 
 namespace Veggie.Controllers {
     public class RegisterController : Controller {
 
         public IActionResult Index() {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection) {
+            RequestVeggieController request = new RequestVeggieController();
+            try {
+                var newUser = new User {
+
+                    username = collection["username"],
+                    password = collection["password"],
+                    nameUser = collection["name"],
+                    lastNameUser = collection["lastname"],
+                    emailUser = collection["email"],
+                };
+                request.createUser(newUser);
+                return View();
+            }catch {
+                return View();
+            }
         }
     }
 }
