99c3a6a16ba55a40ebb29b642cfeb492a55ea9ef
victorisimoo <vmelendrez2013241@gmail.com>
2020-11-25 00:48:07 -0600

    update SDES and DiffieHellman

--- VeggieBack/Controllers/DiffieHellman.cs
+++ VeggieBack/Controllers/DiffieHellman.cs
@@ -3,19 +3,31 @@ using System.Numerics;
 
 namespace VeggieBack.Controllers {
     public class DiffieHellman {
-        public int GenerateKeys(int info) {
+
+        int public_one = 0;
+        int public_two = 0;
+
+        public void GenerateKeys(int info) {
             var g = new BigInteger(43);
             var p = new BigInteger(107);
 
             var a = new BigInteger(new Random().Next());
             var b = new BigInteger(info);
 
-            var secretKey = (int)BigInteger.ModPow(g, b, p);
-            var publicKey = (int)BigInteger.ModPow(g, a, p);
-            return ObtainsKey(secretKey, publicKey);
+            this.public_one = (int)BigInteger.ModPow(g, b, p);
+            this.public_two = (int)BigInteger.ModPow(g, a, p);
         }
-        public static int ObtainsKey(BigInteger privada, BigInteger publica) {
+
+        public int getPrivateKey(BigInteger privada, BigInteger publica) {
             return (int)BigInteger.ModPow(publica, privada, new BigInteger(107));
+        }
+
+        public int getPublicOne(){
+            return public_one;
+        }
+
+        public int getPublicTwo(){
+            return public_two;
         }
     }
 }
