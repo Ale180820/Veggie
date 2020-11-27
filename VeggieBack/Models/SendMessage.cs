31b11017eb16a7de0167d7791fc3494058b205a4
victorisimoo <vmelendrez2013241@gmail.com>
2020-11-27 10:27:10 -0600

    update local branch

--- VeggieBack/Models/SendMessage.cs
+++ VeggieBack/Models/SendMessage.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace VeggieBack.Models {
+
     public class SendMessage {
+
         public Message messageSend { get; set; }
         public int idConversation { get; set; }
         public bool typeMessage { get; set; }
